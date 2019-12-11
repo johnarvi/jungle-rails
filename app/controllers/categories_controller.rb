d7025557a67a058b19e0c9a7910f3587bff68704
Johnarvi <arvind.john@mail.utoronto.ca>
2019-12-10 20:02:12 -0500

    start fixing a new feature

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,7 +1,10 @@
 class CategoriesController < ApplicationController
 
   def show
+    # raise 'here' allows for debugging
     @category = Category.find(params[:id])
+    # raise @category.inspect
+    # raise + inpect above shows the ibnstance of the object.
     @products = @category.products.order(created_at: :desc)
   end
 
