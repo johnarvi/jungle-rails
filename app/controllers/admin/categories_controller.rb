296559de13e9ef23dc9f7c4d39671d8390940128
Johnarvi <arvind.john@mail.utoronto.ca>
2019-12-12 12:33:48 -0500

    implemented about route and page

---
+++ app/controllers/admin/categories_controller.rb
@@ -0,0 +1,4 @@
+# class Admin::CategoriesController < ApplicationController
+#   http_basic_authenticate_with :name => ENV['HTTP_BASIC_AUTH_USERNAME'], :password => ENV['HTTP_BASIC_AUTH_PASSWORD']
+
+# end
